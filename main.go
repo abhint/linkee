34f25bdc5d9baf0ef26fe6da921d18929e1b5319
Abhijith N T <notifyabhijith@gmail.com>
2023-06-17 16:35:20 +0530

    Integrate database and middleware with main application 🚀

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"log"
+
+	"github.com/abhint/linkee/database"
+	"github.com/abhint/linkee/middleware"
+	"github.com/abhint/linkee/router"
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
+	db, err := database.NewDatabase("sqlite3", "test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app := echo.New()
 
+	app.Use(middleware.DatabaseMW(db))
+	app.GET("/", router.Index)
+	app.POST("/api", router.RequestHandler)
 	app.Start(":5000")
 }
