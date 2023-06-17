1188d895b7f082d5668098a1867117371c3042bc
Abhijith N T <notifyabhijith@gmail.com>
2023-06-17 16:34:24 +0530

    Refactor RequestHandler and add error handling 🔀

--- router/router.go
+++ router/router.go
@@ -3,9 +3,37 @@ package router
 import (
 	"net/http"
 
+	"github.com/abhint/linkee/database"
 	"github.com/labstack/echo/v4"
 )
 
+type URL string
+
+type UrlStruct struct {
+	URL `json:"url"`
+}
+
 func Index(c echo.Context) error {
+
 	return c.HTML(http.StatusOK, "<h1>Hello</h1>")
+}
+func RequestHandler(c echo.Context) error {
+	mapping := &database.UrlMapping{}
+	db := c.Get("db").(*database.Database)
+	if db == nil {
+		return c.JSON(http.StatusInternalServerError, "Database connection is nil")
+	}
+
+	urlStruct := &UrlStruct{}
+	if err := c.Bind(urlStruct); err != nil {
+		return c.JSON(http.StatusInternalServerError, "Error binding URL")
+	}
+
+	if urlStruct.URL == "" {
+		return c.JSON(http.StatusBadRequest, "URL is empty")
+	}
+	if err := db.UrlMappings.Insert(string(urlStruct.URL), mapping); err != nil {
+		return c.JSON(http.StatusOK, "Error inserting URL")
+	}
+	return c.JSON(http.StatusOK, mapping)
 }
